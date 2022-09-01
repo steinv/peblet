663e8712d761bba33718299a32f5867a9490db4b
stein <stein.vanmassenhove@gmail.com>
2022-09-01 12:13:24 +0200

    add portfolioproject component

---
+++ src/app/portfolio/portfolio-project/portfolio-project.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'peblet-portfolio-project',
+  templateUrl: './portfolio-project.component.html',
+  styleUrls: ['./portfolio-project.component.scss']
+})
+export class PortfolioProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioListItemComponent } from './portfolio-list-item/portfolio-list-item.component';
+import { PortfolioProjectComponent } from './portfolio-project/portfolio-project.component';
 
 const routes: Routes = [
   { path: '', component: PortfolioComponent },
-  { path: ':project', component: PortfolioComponent },
+  { path: ':project', component: PortfolioProjectComponent },
 ]
 
 @NgModule({
   declarations: [
     PortfolioComponent,
-    PortfolioListItemComponent
+    PortfolioListItemComponent,
+    PortfolioProjectComponent
   ],
   imports: [
     CommonModule,
