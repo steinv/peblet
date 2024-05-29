f7c23feb71a7290a71ec17f7d3a0f1e188643b4d
Stein Vanmassenhove <stein.vanmassenhove@gmail.com>
2024-05-29 16:40:06 +0200

    update to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { DatePipe } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TranslocoPersistLangModule, TRANSLOCO_PERSIST_LANG_STORAGE } from '@ngneat/transloco-persist-lang';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,28 +14,30 @@ import { StatusBarComponent } from './status-bar/status-bar.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeScreenComponent,
-    AppWidgetComponent,
-    NavigationComponent,
-    StatusBarComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule,
-    HttpClientModule,
-    TranslocoRootModule,
-    TranslocoPersistLangModule.forRoot({
-      getLangFn: ({cachedLang, browserLang, defaultLang}) => cachedLang || browserLang || defaultLang,
-      storage: {
-        provide: TRANSLOCO_PERSIST_LANG_STORAGE,
-        useValue: localStorage,
-      },
-    }),
-  ],
-  providers: [DatePipe],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeScreenComponent,
+        AppWidgetComponent,
+        NavigationComponent,
+        StatusBarComponent,
+    ],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FontAwesomeModule,
+        TranslocoRootModule,
+        TranslocoPersistLangModule.forRoot({
+            getLangFn: ({ cachedLang, browserLang, defaultLang }) => cachedLang || browserLang || defaultLang,
+            storage: {
+                provide: TRANSLOCO_PERSIST_LANG_STORAGE,
+                useValue: localStorage,
+            },
+        })
+    ], 
+    providers: [
+        DatePipe, 
+        provideHttpClient(withInterceptorsFromDi())
+    ]
 })
 export class AppModule { }
